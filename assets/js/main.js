162ccd6c0bab391c4c9258945b3130997c65a55a
sagarpyakurel <41280654+sagarpyakurel@users.noreply.github.com>
2026-05-29 23:36:11 -0400

    Clean up unused code: remove duplicate comments in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,31 +211,7 @@
 
 })(jQuery);
 
-
-
-
-
-
-
-
-
-
-
-// function for awards
-// function for awards
-// function for awards
-// function for awards
-// function for awards// function for awards
-// function for awards
-// function for awards// function for awards
-// function for awards// function for awards
-// function for awards// function for awards
-// function for awards
-// function for awards
-
-
-
-
+// Function for award image expansion
 function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
   var imgText = document.getElementById("imgtext");
